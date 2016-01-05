d2d72ae830638763a3192ddc9ca58fe8004aafa4
Fabio Pinelli <fabio.pinelli@gmail.com>
2016-01-05 16:34:32 +0100

    created a new db

--- src/createdb.py
+++ src/createdb.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 import httplib
 import sys
-
+from unidecode import unidecode
 # path = sys.argv[1]
 
-conn = httplib.HTTPSConnection("eztv.ch")
+conn = httplib.HTTPSConnection("eztv.ag")
 	
 headers = {'X-Requested-With': 'XMLHttpRequest', 'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
 
@@ -16,7 +16,7 @@ soup = BeautifulSoup(data,"html5lib")
 
 # soup = BeautifulSoup(open("untitled.html"))
 l = soup.find('select')
-out = open ("db.txt", mode="w")
+out = open ("../support/db.txt", mode="w")
 for serie in l.findAll("option")[1:]:
-	out.write(serie["value"] + "," + serie.contents[0] + "\n")
+	out.write(serie["value"] + "," + unidecode(serie.contents[0]) + "\n")
 
