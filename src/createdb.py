fe451f225cc082eba37439d8b2bdac620a0fb2a0
Fabio Pinelli <fabio.pinelli@gmail.com>
2016-10-11 20:56:01 +0000

    new createdb

--- src/createdb.py
+++ src/createdb.py
@@ -1,22 +1,16 @@
-from bs4 import BeautifulSoup
 import httplib
-import sys
-from unidecode import unidecode
-# path = sys.argv[1]
-
 conn = httplib.HTTPSConnection("eztv.ag")
-	
+import json
+import unidecode
 headers = {'X-Requested-With': 'XMLHttpRequest', 'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
 
-req = conn.request("GET", "/")
+req = conn.request("GET", "/js/search_shows1.js")
 res = conn.getresponse()
 data = res.read()
-# print data
-soup = BeautifulSoup(data,"html5lib")	
 
-# soup = BeautifulSoup(open("untitled.html"))
-l = soup.find('select')
-out = open ("../support/db.txt", mode="w")
-for serie in l.findAll("option")[1:]:
-	out.write(serie["value"] + "," + unidecode(serie.contents[0]) + "\n")
 
+j = json.loads(data.strip().split("=")[-1].replace(";$(document).ready(function(){$(\".tv-show-search-select\").select2({data:data});});",""))
+
+with open("../support/db.txt","w") as fout:
+    for s in j:
+        fout.write(s["id"]+","+unidecode.unidecode(s["text"])+"\n")
